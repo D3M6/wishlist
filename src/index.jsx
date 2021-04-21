2d46c08cf7fd568d9ee817fb148e1a924759b4e4
D3M6 <demg.working@gmail.com>
2021-04-21 12:43:12 +0200

    Modified: Linting from Editor (ESlint activated)

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <div>My Wishlist from REACT</div>,
-    document.getElementById('root')
+  <div>My Wishlist from REACT</div>,
+  document.getElementById('root'),
 );
