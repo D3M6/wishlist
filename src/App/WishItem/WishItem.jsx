3ab8eb7e8b9f94ee149ac0eb7474003f9ad5785a
D3M6 <demg.working@gmail.com>
2021-04-22 11:22:15 +0200

    Finish: React for beginners project

--- src/App/WishItem/WishItem.jsx
+++ src/App/WishItem/WishItem.jsx
@@ -1,24 +1,46 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const WishItem = ({ done, text, id, onDoneChange }) => (
-  <li
-    key={id}
-    className={classNames('wish-list__item', {
-      'wish-list__item--done': done,
-    })}
-  >
-    <input 
-      id={id} 
-      name={id} 
-      checked={done} 
-      type="checkbox"
-      onChange={e => onDoneChange(e.target.checked)}
-    />
-    <label htmlFor={id}>{text}</label>
-  </li>
-);
+const WishItem = ({ done, text, id, onDoneChange }) => {
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    let ageInterval;
+    if (done) {
+      setAge(0)
+    } else { 
+      ageInterval = setInterval(() => {
+        if (done) {
+          clearInterval(ageInterval);
+        } else {
+          setAge(a => a + 1);
+        }
+      }, 1000)
+    }
+    return () => clearInterval(ageInterval);
+  }, [done]);
+  
+  return (
+    <li
+      key={id}
+      className={classNames('wish-list__item', {
+        'wish-list__item--done': done,
+        'wish-list__item--warning': age >= 5 && age < 10,
+        'wish-list__item--danger': age >= 10,
+      })}
+    >
+      <input 
+        id={id} 
+        name={id} 
+        checked={done} 
+        type="checkbox"
+        onChange={e => onDoneChange(e.target.checked)}
+      />
+      <label htmlFor={id}>{text}</label>
+    </li>
+  );
+};
 
 WishItem.propTypes = {
   id: PropTypes.string,
